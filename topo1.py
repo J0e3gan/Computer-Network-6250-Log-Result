603522f50e39608a36932597b1083babebd9fb11
Myung Kim <bpmmhk91@gmail.com>
2015-06-14 14:27:55 -0400

    s stands for completely unsorted and t stands for distances sorted for each node

--- topo1.py
+++
@@ -1,6 +0,0 @@
-# Topology is very simple:
-# A --- B --- C
-
-topo = { 'A' : ['B'], 
-         'B' : ['A', 'C'],
-         'C' : ['B'] }
--- topo12.py
+++
@@ -1,9 +0,0 @@
-# Topology is very simple:
-# A --- B --- C
-# D --- E --- F
-topo = { 'A' : ['B'],
-         'B' : ['A', 'C'],
-         'C' : ['B'],
-         'D' : ['E'], 
-         'E' : ['D', 'F'],
-         'F' : ['E'] }
